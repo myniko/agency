c3411af19bd28348222d83e09fa805090eac0efd
Dominik Mądro <d.madro97@gmail.com>
2017-08-20 20:23:00 +0200

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    $(".owl-carousel").owlCarousel({
+    autoplay:true,
+        loop:true,
+        margin:0,
+        responsive:{
+            0:{items:2},
+            600:{items:2},
+            1000:{items:2},
+        }
+})              
+})
+
+//Copy and paste in to your js Code
+jQuery(document).ready(function(){
+    jQuery(".amazingslider-bullet-text-0").remove();
+    jQuery(".amazingslider-bullet-image-0 img").css("height","100%");
+    jQuery(".amazingslider-bullet-image-0").css("height","100%");
+});
